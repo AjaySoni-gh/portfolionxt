0a224ddbdfdcee47d41d66254d46011a09b43cd0
Ajay soni <144725314+AjaySoni-gh@users.noreply.github.com>
2024-11-01 13:56:29 -0400

    header footr

--- components/ui/Providers.tsx
+++ components/ui/Providers.tsx
@@ -5,7 +5,8 @@ import { ThemeProvider, useTheme } from 'next-themes'
 export default function Providers({children}:{children: React.ReactNode} ) {
   return (
    <ThemeProvider attribute='class'
-   defaultTheme='syatem'>
+   defaultTheme='syatem'
+   disableTransitionOnChange>
     {children}
    </ThemeProvider>
   )
